f2ec43f08451d8a83dac2b5ff767d7fc1a19b316
xinZhang <xin.zhang@geneegroup.com>
2025-01-03 11:06:39 +0800

    ✨ feat(多余文件):

--- server/auth/dist/Nacos/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const startNacos: (serverName: string) => Promise<{
-    port: number;
-}>;
-export declare const getNacosConfig: ({ name, group, }: {
-    name: string;
-    group?: string;
-}) => Promise<any>;
--- server/auth/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const yml = require("yaml");
-const fs = require("fs");
-const file = fs.readFileSync('../config.yml', 'utf8');
-const config = yml.parse(file);
-exports.default = config;
-//# sourceMappingURL=config.js.map
